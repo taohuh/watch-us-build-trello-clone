9078079fa8e33b607a6f2e5650269a5012499c56
taohuh <pimpakarn@maqe.com>
2020-04-20 16:56:13 +0700

    finished

---
+++ src/components/AppDrag.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    draggable
+    @dragstart.self="onDrag"
+    @dragover.prevent
+    @dragenter.prevent
+  >
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    transferData: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    onDrag (e) {
+      e.dataTransfer.effectAllow = 'move'
+      e.dataTransfer.dropAllow = 'move'
+
+      e.dataTransfer.setData('payload', JSON.stringify(this.transferData))
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
